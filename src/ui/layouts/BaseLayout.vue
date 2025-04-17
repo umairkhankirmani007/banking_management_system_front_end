16e33dfae8bc7b663f8d0674311c39bb7e409d06
Abdullah-EF <abdullahhussain@earnflex.com>
2025-04-17 13:30:13 +0500

    ui fixes

--- src/ui/layouts/BaseLayout.vue
+++ src/ui/layouts/BaseLayout.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <header class="bg-gray-100 p-4 shadow">My App Header</header>
     <main class="flex-1 p-4">
       <slot />
     </main>
-    <footer class="bg-gray-100 p-4 text-center">Footer</footer>
   </div>
 </template>
